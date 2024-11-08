697e5ef86b1e26b1ea9bccd78389ce6f9f77e06e
Simone Cecire <ceciresimone@itis-molinari.eu>
2024-11-08 16:10:57 +0100

    aggiunto commento

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const formContainer = document.getElementById("formContainer");
 const mapContainer = document.getElementById("map");
 
 const map = generateMap(mapContainer);
-map.build([45.4639102, 9.1906426]);
+map.build([45.4639102, 9.1906426]); // default viene usato il Duomo di Milano
 map.render();
