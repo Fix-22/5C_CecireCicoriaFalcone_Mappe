808d12402b7cffa7f032216f72eeb67708b57c19
Simone Cecire <ceciresimone@itis-molinari.eu>
2024-11-08 11:48:45 +0100

    aggiunti metodi per creazione mappa

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import {generateForm} from "./formComponent/formComponent.js"
 import {generateGeoencoder} from "./geoencoderComponent/geoencoderComponent.js"
 import {generateMap} from "./mapComponent/mapComponent.js"
+
+const formContainer = document.getElementById("formContainer");
+const mapContainer = document.getElementById("map");
+
+const map = generateMap(mapContainer);
+map.build([45.4639102, 9.1906426]);
+map.render();
--- mapComponent/mapComponent.js
+++ mapComponent/mapComponent.js
@@ -1,5 +1,25 @@
-import L from "leaflet"
-
 export const generateMap = (parentElement) => {
-
+    let map;
+    let places = [];
+    
+    return {
+        build: (startCoords) => {
+            map = L.map(parentElement).setView(startCoords, 14);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+             }).addTo(map);
+        },
+        render: () => {
+            places.forEach((place) => {
+                const marker = L.marker(place.coords).addTo(map);
+                marker.bindPopup("<b>" + place.name + "</b>");
+             });
+        },
+        addPlace: (place) => {
+            if (places.indexOf(place) === -1) {
+                places.push(place);
+            }
+        }
+    };
 };
